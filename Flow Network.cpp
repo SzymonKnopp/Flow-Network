e4f9b558e651b29fa2f3c9d2880dac1199af38fc
Szymon Knopp <knopp.s99@gmail.com>
2020-05-22 21:06:09 +0200

    ford-fulkerson.h added

--- Flow Network.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "node.h"
-
-using namespace std;
-
-int main() {
-	Node* source = new Node(0);
-}
---
+++ ford-fulkerson.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "node.h"
+
+
+std::vector<pipe*> pathToAugment(Node* source);
+
+// fills flow network with maximum flow
+void fordFulkerson(Node* source) {
+	while (true) {
+
+	}
+}
+
+std::vector<pipe*> pathToAugment(Node* source) {
+
+}
