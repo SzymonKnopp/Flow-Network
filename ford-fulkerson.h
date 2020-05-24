caaa7328d209fd260d9ab137348834df87a729c1
Szymon Knopp <knopp.s99@gmail.com>
2020-05-24 17:45:21 +0200

    added DFS

--- ford-fulkerson.h
+++ ford-fulkerson.h
@@ -1,21 +1,19 @@
 #pragma once
-#include <stack>
 #include "node.h"
 
 using namespace std;
 
 void augmentPipe(pipe* pipe, int value);
 int bottleneckValue(vector<pipe*> path);
-vector<pipe*> stackToVector(stack<pipe*> stack);
 void augmentPath(vector<pipe*> path);
-stack<pipe*> pathToAugment(Node* source);
+bool foundPath(vector<pipe*>& path, Node* node);
+vector<pipe*> pathToAugment(Node* source);
 
 // fills flow network with maximum flowS
 void fordFulkerson(Node* source) {
 	while (true) {
 		try {
-			stack<pipe*> pathStack = pathToAugment(source);
-			vector<pipe*> path = stackToVector(pathStack);
+			vector<pipe*> path = pathToAugment(source);
 			augmentPath(path);
 		}
 		catch (...) {
@@ -24,18 +22,39 @@ void fordFulkerson(Node* source) {
 	}
 }
 
-stack<pipe*> pathToAugment(Node* source) throw (int) {
-	stack<pipe*> path;
-	Node* current = source;
-	while (!current->isSink()) {
-		try {
-			current = current->goThrough(path);
-		}
-		catch (...) {
-			throw -1; //dead-end, no viable path
+vector<pipe*> pathToAugment(Node* source) throw (int) {
+	vector<pipe*> path;
+	if (foundPath(path, source)) {
+		return path;
+	}
+	else {
+		throw -1; //viable path not found
+	}
+}
+
+bool foundPath(vector<pipe*>& path, Node* node) {
+	if (node->isSink()) {
+		return true;
+	}
+	for (auto& pipe : node->pipesOut) {
+		if (pipe->free() != 0
+			&& node->step <= pipe->sink->step &&
+			foundPath(path, pipe->sink)) {
+				pipe->pathDir = pathDir_t::toSink;
+				path.push_back(pipe);
+				return true;
 		}
 	}
-	return path;
+	for (auto& pipe : node->pipesIn) {
+		if (pipe->used != 0
+			&& node->step <= pipe->source->step &&
+			foundPath(path, pipe->source)) {
+				pipe->pathDir = pathDir_t::toSource;
+				path.push_back(pipe);
+				return true;
+		}
+	}
+	return false;
 }
 
 void augmentPath(vector<pipe*> path) {
@@ -43,16 +62,6 @@ void augmentPath(vector<pipe*> path) {
 	for (auto& pipe : path) {
 		augmentPipe(pipe, bottleneckVal);
 	}
-}
-
-vector<pipe*> stackToVector(stack<pipe*> stack) {
-	vector<pipe*> vector;
-	int size = stack.size();
-	for (int i = 0; i < size; i++) {
-		vector.push_back(stack.top());
-		stack.pop();
-	}
-	return vector;
 }
 
 int bottleneckValue(vector<pipe*> path) {
